6a06a276f44355c7e53e4bd72372d641c27c1c7d
MarkusMcNugen <marknewton5@gmail.com>
2018-02-03 02:32:25 -0500

    Update iptables.sh

--- qbittorrent/iptables.sh
+++ qbittorrent/iptables.sh
@@ -26,7 +26,7 @@ for lan_network_item in "${lan_network_list[@]}"; do
 
 done
 
-echo "[info] ip route defined as follows..."
+echo "[info] ip route defined as follows..." | ts '%Y-%m-%d %H:%M:%.S'
 echo "--------------------"
 ip route
 echo "--------------------"
@@ -44,7 +44,7 @@ iptable_mangle_exit_code=$?
 
 if [[ $iptable_mangle_exit_code == 0 ]]; then
 
-	echo "[info] iptable_mangle support detected, adding fwmark for tables"
+	echo "[info] iptable_mangle support detected, adding fwmark for tables" | ts '%Y-%m-%d %H:%M:%.S'
 
 	# setup route for deluge webui using set-mark to route traffic for port 8112 to eth0
 	echo "8080    webui" >> /etc/iproute2/rt_tables
@@ -73,7 +73,7 @@ fi
 
 # convert netmask into cidr format
 docker_network_cidr=$(ipcalc "${docker_ip}" "${docker_mask}" | grep -P -o -m 1 "(?<=Network:)\s+[^\s]+")
-echo "[info] Docker network defined as ${docker_network_cidr}"
+echo "[info] Docker network defined as ${docker_network_cidr}" | ts '%Y-%m-%d %H:%M:%.S'
 
 # input iptable rules
 ###
@@ -162,7 +162,7 @@ iptables -A OUTPUT -p icmp --icmp-type echo-request -j ACCEPT
 # accept output from local loopback adapter
 iptables -A OUTPUT -o lo -j ACCEPT
 
-echo "[info] iptables defined as follows..."
+echo "[info] iptables defined as follows..." | ts '%Y-%m-%d %H:%M:%.S'
 echo "--------------------"
 iptables -S
 echo "--------------------"
