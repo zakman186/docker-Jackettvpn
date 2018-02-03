ef5022a7b79b88dcf38e9444e21c1c8df15ae629
MarkusMcNugen <marknewton5@gmail.com>
2018-02-02 22:35:08 -0500

    Update start.sh

--- qbittorrent/start.sh
+++ qbittorrent/start.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+ function trap_handler
+ {
+     echo "[info] Shutdown detected... copying config file to /config/qbittorrent"
+     yes | cp /root/.config/qBittorrent/qBittorrent.conf /config/qbittorrent/qBittorrent.conf
+ }
+
 # if config file doesnt exist then copy default config file
 if [[ ! -f /config/qbittorrent/qBittorrent.conf ]]; then
 
-	echo "qBittorrent config file does not exist, copying default settings to /config/qBittorrent"
-	echo "You can edit the conf file at /config/qBittorrent to change qBittorrents settings while docker if offline"
+	echo "[warn] qBittorrent config file does not exist, copying default settings to /config/qBittorrent"
+	echo "[info] You can edit the conf file at /config/qBittorrent to change qBittorrents settings while docker if offline"
 	yes | cp /home/$USER/.config/qBittorrent/qBittorrent.conf /config/qbittorrent/qBittorrent.conf
 	chown -R "${PUID}":"${PGID}" /config/qbittorrent
 	chmod -R 775 /config/qbittorrent
@@ -17,5 +23,8 @@ else
 
 fi
 
-echo "Starting qBittorrent daemon..."
+trap trap_handler SIGINT SIGTERM SIGHUP 
+
+echo "[info] Starting qBittorrent daemon..."
 /usr/bin/qbittorrent-nox -d
+wait
