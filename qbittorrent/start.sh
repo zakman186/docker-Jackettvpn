feb7c06f1dafba5e6de9acf756bb06aab19572af
MarkusMcNugen <marknewton5@gmail.com>
2018-02-02 20:42:15 -0500

    Update start.sh

--- qbittorrent/start.sh
+++ qbittorrent/start.sh
@@ -6,11 +6,14 @@ if [[ ! -f /config/qbittorrent/qBittorrent.conf ]]; then
 	echo "qBittorrent config file does not exist, copying default settings to /config/qBittorrent"
 	echo "You can edit the conf file at /config/qBittorrent to change qBittorrents settings"
 	cp /home/$USER/.config/qBittorrent/qBittorrent.conf /config/qbittorrent/qBittorrent.conf
+	chown -R "${PUID}":"${PGID}" /config/qbittorrent
+	chmod -R 775 /config/qbittorrent
 
 else
 
 	echo "qBittorrent config file exists, copying to config directory"
 	cp /config/qbittorrent/qBittorrent.conf /home/$USER/.config/qBittorrent/qBittorrent.conf
+
 	chmod 644 /home/$USER/.config/qBittorrent/qBittorrent.conf
 
 fi
