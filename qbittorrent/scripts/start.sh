953f14250880e0dacf18d2eb0e8208f436005dfd
MarkusMcNugen <marknewton5@gmail.com>
2018-02-03 05:10:50 -0500

    Update start.sh

--- qbittorrent/scripts/start.sh
+++ qbittorrent/scripts/start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-function trap_handler
-{
-	echo "[info] Shutdown detected... copying config file to /config/qbittorrent" | ts '%Y-%m-%d %H:%M:%.S'
-	yes | cp /root/.config/qBittorrent/qBittorrent.conf /config/qbittorrent/qBittorrent.conf
-}
+#function trap_handler
+#{
+#	echo "[info] Shutdown detected... copying config file to /config/qbittorrent" | ts '%Y-%m-%d %H:%M:%.S'
+#	yes | cp /root/.config/qBittorrent/qBittorrent.conf /config/qbittorrent/qBittorrent.conf
+#}
 
 # Make qbittorrent config directory
 mkdir -p /config/qbittorrent
@@ -27,7 +27,7 @@ else
 	chmod 644 /root/.config/qBittorrent/qBittorrent.conf
 fi
 
-trap trap_handler SIGINT SIGTERM SIGHUP 
+#trap trap_handler SIGINT SIGTERM SIGHUP 
 
 echo "[info] Starting qBittorrent daemon..." | ts '%Y-%m-%d %H:%M:%.S'
 /usr/bin/qbittorrent-nox -d 
