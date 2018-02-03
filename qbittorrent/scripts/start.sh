22bac8e8dfdfa65c0395dca0bc492f6ae7f0f163
MarkusMcNugen <marknewton5@gmail.com>
2018-02-03 05:05:35 -0500

    Update start.sh

--- qbittorrent/scripts/start.sh
+++ qbittorrent/scripts/start.sh
@@ -15,8 +15,10 @@ if [[ ! -f /config/qbittorrent/qBittorrent.conf ]]; then
 	echo "[warn] qBittorrent config file does not exist, copying default settings to /config/qbittorrent" | ts '%Y-%m-%d %H:%M:%.S'
 	echo "[info] You can edit the conf file at /config/qbittorrent to change qBittorrents settings and restart the container" | ts '%Y-%m-%d %H:%M:%.S'
 	yes | cp /etc/qbittorrent/default/qBittorrent.conf /config/qbittorrent/qBittorrent.conf
+	yes | cp /config/qbittorrent/qBittorrent.conf /root/.config/qBittorrent/qBittorrent.conf
 	chown -R "${PUID}":"${PGID}" /config/qbittorrent
 	chmod -R 775 /config/qbittorrent
+	chmod 644 /root/.config/qBittorrent/qBittorrent.conf
 # Else create directories and copy conf from config volume
 else
 	echo "qBittorrent config file exists in /config, copying to qbittorrent config directory" | ts '%Y-%m-%d %H:%M:%.S'
@@ -29,5 +31,4 @@ fi
 trap trap_handler SIGINT SIGTERM SIGHUP 
 
 echo "[info] Starting qBittorrent daemon..." | ts '%Y-%m-%d %H:%M:%.S'
-/usr/bin/qbittorrent-nox -d &
-wait
+/usr/bin/qbittorrent-nox -d 
