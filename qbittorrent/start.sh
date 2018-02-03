2a56a35f73d5116b8a9f815a88559a52b5e17332
MarkusMcNugen <marknewton5@gmail.com>
2018-02-03 02:19:46 -0500

    Update start.sh

--- qbittorrent/start.sh
+++ qbittorrent/start.sh
@@ -2,30 +2,34 @@
 set -x
 
 function trap_handler
- {
-     echo "[info] Shutdown detected... copying config file to /config/qbittorrent"
-     yes | cp /root/.config/qBittorrent/qBittorrent.conf /config/qbittorrent/qBittorrent.conf
- }
+{
+	echo "[info] Shutdown detected... copying config file to /config/qbittorrent"
+	yes | cp /root/.config/qBittorrent/qBittorrent.conf /config/qbittorrent/qBittorrent.conf
+}
 
 # if config file doesnt exist then copy default config file
 if [[ ! -f /config/qbittorrent/qBittorrent.conf ]]; then
-
 	echo "[warn] qBittorrent config file does not exist, copying default settings to /config/qbittorrent"
 	echo "[info] You can edit the conf file at /config/qbittorrent to change qBittorrents settings and restart the container"
 	yes | cp /root/.config/qBittorrent/qBittorrent.conf /config/qbittorrent/qBittorrent.conf
 	chown -R "${PUID}":"${PGID}" /config/qbittorrent
 	chmod -R 775 /config/qbittorrent
-
 else
-
 	echo "qBittorrent config file exists in /config, copying to qbittorrent config directory"
 	yes | cp /config/qbittorrent/qBittorrent.conf /root/.config/qBittorrent/qBittorrent.conf
 	chmod 644 /root/.config/qBittorrent/qBittorrent.conf
-
 fi
+
+while true; do
+	if [ -c /dev/net/tun ];
+		exec /bin/bash /etc/qbittorrent/iptables.sh
+		break
+	else
+		sleep 5
+	fi
+done
 
 trap trap_handler SIGINT SIGTERM SIGHUP 
 
 echo "[info] Starting qBittorrent daemon..."
-/usr/bin/qbittorrent-nox -d
-wait
+/usr/bin/qbittorrent-nox -d &&; wait
