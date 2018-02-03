896045ba34ce5c1a21cc18439a6bed85105d3273
MarkusMcNugen <marknewton5@gmail.com>
2018-02-03 01:03:04 -0500

    Update start.sh

--- qbittorrent/start.sh
+++ qbittorrent/start.sh
@@ -9,17 +9,17 @@
 # if config file doesnt exist then copy default config file
 if [[ ! -f /config/qbittorrent/qBittorrent.conf ]]; then
 
-	echo "[warn] qBittorrent config file does not exist, copying default settings to /config/qBittorrent"
-	echo "[info] You can edit the conf file at /config/qBittorrent to change qBittorrents settings while docker if offline"
-	yes | cp /home/$USER/.config/qBittorrent/qBittorrent.conf /config/qbittorrent/qBittorrent.conf
+	echo "[warn] qBittorrent config file does not exist, copying default settings to /config/qbittorrent"
+	echo "[info] You can edit the conf file at /config/qbittorrent to change qBittorrents settings and restart the container"
+	yes | cp /root/.config/qBittorrent/qBittorrent.conf /config/qbittorrent/qBittorrent.conf
 	chown -R "${PUID}":"${PGID}" /config/qbittorrent
 	chmod -R 775 /config/qbittorrent
 
 else
 
 	echo "qBittorrent config file exists in /config, copying to qbittorrent config directory"
-	yes | cp /config/qbittorrent/qBittorrent.conf /home/$USER/.config/qBittorrent/qBittorrent.conf
-	chmod 644 /home/$USER/.config/qBittorrent/qBittorrent.conf
+	yes | cp /config/qbittorrent/qBittorrent.conf /root/.config/qBittorrent/qBittorrent.conf
+	chmod 644 /root/.config/qBittorrent/qBittorrent.conf
 
 fi
 
