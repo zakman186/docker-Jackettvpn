c127af5bb1b4171437c2534638e8c5e475d898b3
MarkusMcNugen <marknewton5@gmail.com>
2018-02-03 23:44:34 -0500

    Update start.sh

--- qbittorrent/start.sh
+++ qbittorrent/start.sh
@@ -4,17 +4,16 @@ set -e
 _handler() {
 	echo "[warn] Shutdown detected... cleaning up real quick!" | ts '%Y-%m-%d %H:%M:%.S'
 	# if config directory exists, apply permissions before exiting
-	if [[ -e /config/qBittorrent ]]; then
-		echo "[info] qBittorrent directory exists in /config, applying ownership and permissions before exit" | ts '%Y-%m-%d %H:%M:%.S'
-		chmod -R 755 /config/qBittorrent
-		chown -R ${PUID}:${PGID} /config/qBittorrent
-	fi
+	chmod -R 755 /config/qBittorrent
 }
 
 trap _handler SIGINT SIGTERM SIGHUP 
 
 if [[ ! -e /config/qBittorrent ]]; then
 	mkdir -p /config/qBittorrent/config/
+	chown -R ${PUID}:${PGID} /config/qBittorrent
+else
+	chown -R ${PUID}:${PGID} /config/qBittorrent
 fi
 
 if [[ ! -e /config/qBittorrent/config/qBittorrent.conf ]]; then
@@ -25,16 +24,6 @@ fi
 
 echo "[info] Starting qBittorrent daemon..." | ts '%Y-%m-%d %H:%M:%.S'
 /etc/qbittorrent/qbittorrent.init start
-
-while true; do
-	if [ -e /config/qBittorrent ]; then
-		chmod -R 755 /config/qBittorrent
-		chown -R ${PUID}:${PGID} /config/qBittorrent
-		break
-	else
-		sleep 1
-	fi
-done
 
 child=$(pgrep -o -x qbittorrent-nox) 
 wait "$child"
