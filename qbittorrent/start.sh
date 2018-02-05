acd84665eab18766f5a42ff2e65c1ae30dd738f0
MarkusMcNugen <marknewton5@gmail.com>
2018-02-04 23:04:39 -0500

    Update start.sh

--- qbittorrent/start.sh
+++ qbittorrent/start.sh
@@ -1,24 +1,6 @@
 #!/bin/bash
 set -e
 
-_handler() {
-	echo "[warn] Shutdown detected... cleaning up real quick!" | ts '%Y-%m-%d %H:%M:%.S'
-	# if config directory exists, apply permissions before exiting
-	qbpid=$(pgrep -o -x qbittorrent-nox)
-	if [[ ! -z $qbpid ]]; then
-		echo "[warn] qBittorrent still running... shutting down now" | ts '%Y-%m-%d %H:%M:%.S'
-		kill -9 $qbpid
-	fi
-	
-	if [[ -e /config/qBittorrent ]]; then
-		echo "[info] qBittorrent directory exists in /config, applying ownership and permissions before exit" | ts '%Y-%m-%d %H:%M:%.S'
-		chmod -R 755 /config/qBittorrent
-		chown -R $PUID:$PGID /config/qBittorrent
-	fi
-}
-
-trap _handler SIGINT SIGTERM SIGHUP 
-
 if [[ ! -e /config/qBittorrent ]]; then
 	mkdir -p /config/qBittorrent/config/
 	chown -R ${PUID}:${PGID} /config/qBittorrent
@@ -36,12 +18,11 @@ echo "[info] Starting qBittorrent daemon..." | ts '%Y-%m-%d %H:%M:%.S'
 chmod -R 755 /config/qBittorrent
 
 sleep 1
+qbpid=$(pgrep -o -x qbittorrent-nox) 
+echo "[info] qBittorrent PID: $qbpid" | ts '%Y-%m-%d %H:%M:%.S'
 
-echo "[info] qBittorrent PID: $child" | ts '%Y-%m-%d %H:%M:%.S'
-
-if [ -e /proc/$child ]; then
-	this=$(echo $$) 
-	wait "$this"
+if [ -e /proc/$qbpid ]; then
+	sleep infinity
 else
-	echo "qBittorrent failed to start!" || exit 2
+	echo "qBittorrent failed to start!"
 fi
