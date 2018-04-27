382749d0b471b99cc33ed5afcecf6609a0f7419e
MarkusMcNugen <marknewton5@gmail.com>
2018-04-26 22:23:49 -0400

    Update start.sh

--- qbittorrent/start.sh
+++ qbittorrent/start.sh
@@ -19,7 +19,7 @@ if [ ! -z "${WEBUI_PORT}" ]; then
 		if [[ ! -z "${webui_exist}" ]]; then
 			# Get line number of WebUI Port
 			LINE_NUM=$(grep -Fn -m 1 'WebUI\Port' /config/qBittorrent/config/qBittorrent.conf | cut -d: -f 1)
-			sed -i "${LINE_NUM}s@^@WebUI\Port=${WEBUI_PORT}\n@" /config/qBittorrent/config/qBittorrent.conf
+			sed -i "${LINE_NUM}s@.*@WebUI\Port=${WEBUI_PORT}\n@" /config/qBittorrent/config/qBittorrent.conf
 		else
 			echo "WebUI\Port=${WEBUI_PORT}" >> /config/qBittorrent/config/qBittorrent.conf
 		fi
@@ -33,7 +33,7 @@ if [ ! -z "${INCOMING_PORT}" ]; then
 		if [[ ! -z "${incoming_exist}" ]]; then
 			# Get line number of Incoming
 			LINE_NUM=$(grep -Fn -m 1 'Connection\PortRangeMin' /config/qBittorrent/config/qBittorrent.conf | cut -d: -f 1)
-			sed -i "${LINE_NUM}s@^@Connection\PortRangeMin=${INCOMING_PORT}\n@" /config/qBittorrent/config/qBittorrent.conf
+			sed -i "${LINE_NUM}s@.*@Connection\PortRangeMin=${INCOMING_PORT}\n@" /config/qBittorrent/config/qBittorrent.conf
 		else
 			echo "Connection\PortRangeMin=${INCOMING_PORT}" >> /config/qBittorrent/config/qBittorrent.conf
 		fi
